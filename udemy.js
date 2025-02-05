7be75bbf599cfbfe6c3af08592b44043c2554205
Nirmal Jaiswal <nirmaljaiswal888@gmail.com>
2025-02-05 16:12:07 +0530

    Updated program with new features and improvements-addToCart button

--- udemy.js
+++ udemy.js
@@ -3,14 +3,30 @@ const cartpage= document.querySelector('.cartPage');
 const addToCartButton= document.querySelectorAll('.cartbutton');
 const cartButton= document.getElementById('cart');
 const keepshoppingbutton =document.getElementById('keepshopping');
+const cartItemsContainer = document.querySelector('.cart-items');
 
+const products= [
+{name: "Web Development Bootcamp",price: 500, quantity: 0},
+{ name:"Computer Basics", price: 899, quantity: 0},
+{ name:"The complete JavaScript Course", price: 500, quantity: 0},
+{ name:"Computer Basics & Excel Analytics", price: 1500, quantity: 0}
+];
 
-cartButton.addEventListener('click', function(){
-  cartpage.style.display ="block";
+addToCartButton.forEach((button, index)=>{
+button.addEventListener('click', function(){
+  cart.push(products[index]);
+  localStorage.setItem("cart", JSON.stringify(cart));
+  alert(`${products[index].name} has been added to your cart!`);
+});
+});
+
+const cart = JSON.parse(localStorage.getItem('cart')) || [];
+ cartButton.addEventListener('click', function(){
+   cartpage.style.display ="block";
   productpage.style.display ="none";
 });
 
 keepshoppingbutton.addEventListener('click', function(){
   cartpage.style.display ="none";
   productpage.style.display ="block";
-})
+});
