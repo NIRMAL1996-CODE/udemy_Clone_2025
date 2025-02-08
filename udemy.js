5d6dd1b8364780223b235a8c87e4b7fa9d192ab4
Nirmal Jaiswal <nirmaljaiswal888@gmail.com>
2025-02-08 11:48:20 +0530

    clone of udemy (javascript)

--- udemy.js
+++ udemy.js
@@ -12,21 +12,46 @@ const products= [
 { name:"Computer Basics & Excel Analytics", price: 1500, quantity: 0}
 ];
 
-addToCartButton.forEach((button, index)=>{
-button.addEventListener('click', function(){
-  cart.push(products[index]);
-  localStorage.setItem("cart", JSON.stringify(cart));
-  alert(`${products[index].name} has been added to your cart!`);
-});
-});
+// addToCartButton.forEach((button, index)=>{
+// button.addEventListener('click', function(){
+//   cart.push(products[index]);
+//   localStorage.setItem("cart", JSON.stringify(cart));
+//   alert(`${products[index].name} has been added to your cart!`);
+// });
+// });
 
 const cart = JSON.parse(localStorage.getItem('cart')) || [];
+
  cartButton.addEventListener('click', function(){
    cartpage.style.display ="block";
   productpage.style.display ="none";
+  displayCartItems();
 });
 
+function displayCartItems(){
+  cartItemsContainer.innerHTML = '';
+  if(cart.length===0){
+    localStorage.removeItem('cart');
+    cartItemsContainer.innerHTML ='<p>Your cart is empty. Keep shopping to find a course!</p>';
+  }
+};
 keepshoppingbutton.addEventListener('click', function(){
   cartpage.style.display ="none";
   productpage.style.display ="block";
 });
+
+// addToCartButton.forEach((button, index)=>{
+//   button.addEventListener('click', function(){
+//     const productName = button.parentElement.children[1].innerText;
+//     const productPrice = button.parentElement.children[2].innerText.split(": ")[1];
+//     const productImageSrc = button.parentElement.children[0].src;
+//     const cartProductData = {
+//       name: productName,price: productPrice, quantity: 1, image: productImageSrc
+//     }
+//     let existingCartData = JSON.parse(localStorage.getItem("cart")) ? JSON.parse(localStorage.getItem("cart")) : [];
+//     existingCartData.push(cartProductData);
+//     localStorage.setItem("cart", JSON.stringify(existingCartData));
+//     alert(`${products[index].name} has been added to your cart!`);
+//   });
+//   });
+
