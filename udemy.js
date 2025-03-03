c737082b5c47062e3c7f699bbcede758f23f02c6
Nirmal Jaiswal <nirmaljaiswal888@gmail.com>
2025-03-03 14:58:31 +0530

    updated javaScript(add to cart button)

--- udemy.js
+++ udemy.js
@@ -5,52 +5,44 @@ const cartButton= document.getElementById('cart');
 const keepshoppingbutton =document.getElementById('keepshopping');
 const cartItemsContainer = document.querySelector('.cart-items');
 
-const products= [
-{name: "Web Development Bootcamp",price: 500, quantity: 0},
-{ name:"Computer Basics", price: 899, quantity: 0},
-{ name:"The complete JavaScript Course", price: 500, quantity: 0},
-{ name:"Computer Basics & Excel Analytics", price: 1500, quantity: 0}
-];
-// addToCartButton.forEach((button, index)=>{
-// button.addEventListener('click', function(){
-//   cart.push(products[index]);
-//   localStorage.setItem("cart", JSON.stringify(cart));
-//   alert(`${products[index].name} has been added to your cart!`);
-// });
-// });
-
-const cart = JSON.parse(localStorage.getItem('cart')) || [];
-
- cartButton.addEventListener('click', function(){
-   cartpage.style.display ="block";
+//cartbutton opens cart page
+cartButton.addEventListener('click', function(){
+  cartpage.style.display ="block";
   productpage.style.display ="none";
   displayCartItems();
 });
-
+//keepshopping button opens product page
+keepshoppingbutton.addEventListener('click', function(){
+  cartpage.style.display ="none";
+  productpage.style.display ="block";
+});
+//updates the cart and if no products is there , tells that cart is empty
 function displayCartItems(){
   cartItemsContainer.innerHTML = '';
   if(cart.length===0){
     localStorage.removeItem('cart');
     cartItemsContainer.innerHTML ='<p>Your cart is empty. Keep shopping to find a course!</p>';
-  }
+     }
 };
-keepshoppingbutton.addEventListener('click', function(){
-  cartpage.style.display ="none";
-  productpage.style.display ="block";
-});
 
-// addToCartButton.forEach((button, index)=>{
-//   button.addEventListener('click', function(){
-//     const productName = button.parentElement.children[1].innerText;
-//     const productPrice = button.parentElement.children[2].innerText.split(": ")[1];
-//     const productImageSrc = button.parentElement.children[0].src;
-//     const cartProductData = {
-//       name: productName,price: productPrice, quantity: 1, image: productImageSrc
-//     }
-//     let existingCartData = JSON.parse(localStorage.getItem("cart")) ? JSON.parse(localStorage.getItem("cart")) : [];
-//     existingCartData.push(cartProductData);
-//     localStorage.setItem("cart", JSON.stringify(existingCartData));
-//     alert(`${products[index].name} has been added to your cart!`);
-//   });
-//   });
+
+addToCartButton.forEach((button)=>{
+  button.addEventListener('click', function(){
+    const productName = button.parentElement.querySelector('h2').innerText;
+    const productPrice = button.parentElement.querySelector('p').innerText.split(": ")[1];
+    const productImageSrc = button.parentElement.querySelector('img').src;
+    
+    const cartProductData = {
+      name: productName,
+      price: productPrice,
+      quantity: 1,
+      image: productImageSrc
+    };
+
+    let existingCartData = JSON.parse(localStorage.getItem("cart")) || [];
+      existingCartData.push(cartProductData);
+      localStorage.setItem("cart", JSON.stringify(existingCartData));
+      alert(`${productName} has been added to your cart!`);
+  });
+});
 
